973692664221d9d956604ff0bc7578dab70b0bdf
fsanchezcl <129914824+fsanchezcl@users.noreply.github.com>
2023-10-01 22:00:03 -0300

    Entrega Final

---
+++ 2- FrontEnd/eval_nutrientes/app.R
@@ -0,0 +1,151 @@
+library(shiny)
+library(jsonlite)
+library(dplyr)
+
+data <- fromJSON("nutrientes.json")
+source("funcionesPropias.R")
+
+# Vista Web
+ui <- fluidPage(
+
+  # Titulo
+  titlePanel("Aporte Nutricional (valor medio en 100g) "),
+
+  sidebarLayout(
+
+    # Panel Ingreso de Aporte Nutricional
+    sidebarPanel(
+      
+      # Ingreso Tipo de Comida
+      selectInput("foodType", "Tipo de alimento:",
+                  choices = data$alimentos$nombre,
+                  selected = data$alimentos$nombre[1]),
+
+      # Ingreso Carbohidratos
+      sliderInput("carbohidratos", "Carbohidratos (g):",
+                  min = 0, max = 500,
+                  value = 0, step = 0.1),
+
+      # Ingreso Proteínas
+      sliderInput("proteinas", "Proteínas (g):",
+                  min = 0, max = 500,
+                  value = 0, step = 0.1),
+
+      # Ingreso Grasas
+      sliderInput("grasas", "Grasas (g):",
+                  min = 0, max = 500,
+                  value = 0, step = 0.1),
+
+      # Ingreso Calorías
+      sliderInput("calorias", "Calorías (Kcal):",
+                  min = 0, max = 500,
+                  value = 0, step = 0.1),
+
+    ),
+
+    mainPanel(
+      column(6,tableOutput("values"),
+      actionButton("evaluar_btn", "Evaluar")),
+      column(6,uiOutput("resultado_evaluacion"))
+
+    )
+  ),
+  
+  tags$footer(
+    HTML("</br>Para cualquier contacto o sugerencia, por favor escriba a: <a href='mailto:eval.nutrientes@fci.cl'>eval.nutrientes@fci.cl</a>")
+  )
+  
+)
+
+# Componente Servidor
+server <- function(input, output, session) {
+
+  sliderValues <- reactive({
+    
+    data.frame(
+      Nutriente = c("Tipo de alimento", "Carbohidratos (g)",
+                    "Proteínas (g)",
+                    "Grasas (g)",
+                    "Calorías (Kcal)"),
+      Valor = as.character(c(input$foodType, input$carbohidratos,
+                             input$proteinas,
+                             input$grasas,
+                             input$calorias)),
+      stringsAsFactors = FALSE)
+    
+  })
+  
+  observar_resultado <- function(alimento, carbo, prot, gras, cal) {
+    result <- encontrar_cercano_cluster(alimento, carbo, prot, gras, cal)
+    return(result)
+  }
+
+  # Variable reactiva para almacenar resultado
+  resultadoReactive <- reactiveVal(0)
+  
+  # Variable para controlar su visibilidad
+  resultadoVisible <- reactiveVal(FALSE)
+    
+  # Oculta el resultado por modificación de parámetros de consulta
+  observe({
+    alimentoSeleccionado <- data$alimentos[data$alimentos$nombre == input$foodType, ]
+    
+    updateSliderInput(session, "carbohidratos", min = alimentoSeleccionado$nutrientes$Carbohidratos$min, max = alimentoSeleccionado$nutrientes$Carbohidratos$max, value=0)
+    updateSliderInput(session, "proteinas", min = alimentoSeleccionado$nutrientes$Proteínas$min, max = alimentoSeleccionado$nutrientes$Proteínas$max, value=0)
+    updateSliderInput(session, "grasas", min = alimentoSeleccionado$nutrientes$Grasas$min, max = alimentoSeleccionado$nutrientes$Grasas$max, value=0)
+    updateSliderInput(session, "calorias", min = alimentoSeleccionado$nutrientes$Calorías$min, max = alimentoSeleccionado$nutrientes$Calorías$max, value=0)
+  })
+  
+  # Reinicia la visibilidad del resultado por modificación de parámetros de consulta
+  observe({
+    input$foodType
+    input$carbohidratos
+    input$proteinas
+    input$grasas
+    input$calorias
+    
+    resultadoVisible(FALSE)
+  })
+  
+  # Muestra el resultado al presionar "Evaluar"
+  observeEvent(input$evaluar_btn, {
+    resultado <- observar_resultado(input$foodType, input$carbohidratos, input$proteinas, input$grasas, input$calorias)
+    resultadoReactive(resultado)
+    resultadoVisible(TRUE)
+  })
+    
+  output$resultado_evaluacion <- renderUI({
+    if (!resultadoVisible()) return(NULL)  # Termina si no se muestra resultado
+    
+    resultado <- resultadoReactive()
+    color <- resultado[[1]]
+    descripcion <- resultado[[2]]
+    HTML(paste0('<div style="border: 5px solid ', color, '; padding: 10px;">',
+                '<span style="font-weight:bold; font-size:20px;">',
+                "El resultado de la evaluación para la consulta de ", input$foodType, " es: ", descripcion, '</span></div>'))
+  })
+  
+  sliderValues <- reactive({
+    
+    data.frame(
+      Nutriente = c("Tipo de alimento", "Carbohidratos (g)",
+                    "Proteínas (g)",
+                    "Grasas (g)",
+                    "Calorías (Kcal)"),
+      Valor = as.character(c(input$foodType, input$carbohidratos,
+                             input$proteinas,
+                             input$grasas,
+                             input$calorias)),
+      stringsAsFactors = FALSE)
+    
+  })
+  
+  # Despliega valores
+  output$values <- renderTable({
+    sliderValues()
+  })
+
+}
+
+# Crea aplicación Shiny
+shinyApp(ui, server)
---
+++ 2- FrontEnd/eval_nutrientes/funcionesPropias.R
@@ -0,0 +1,72 @@
+# Autor: Ricardo Rodriguez
+# Fecha: 30/08/2023
+
+# Changelog
+# 01/09/2023 Felipe Sánchez Fabre (FASF) - Cambio del orden de los parámetros (macronutrientes y micronutriente) y mejoras en la lista a retornar.
+
+
+normalize_column <- function(column, max_value, min_value) {
+  return((column - min_value) / (max_value - min_value))
+}
+
+# Buscar cluster mas cercano a producto
+encontrar_cercano_cluster <- function(TIPO_PRODUCTO, DT_CARBOHYDRATES_100G, DT_PROTEINS_100G, DT_FAT_100G, DT_ENERGY_100G) { #FASF cambio en el orden
+  
+  if (!(TIPO_PRODUCTO %in% c("soja", "tofu", "seitan"))) {
+    stop("Tipo de producto no válido.")
+  }
+  
+  # Leer las descripciones de los clústeres desde un archivo JSON
+  descripciones_clusters <- fromJSON("descripciones_clusters.json")
+  
+  if(TIPO_PRODUCTO == "soja") {
+    max_min <- fromJSON("max_min_soja.json")
+    centroides <- fromJSON("centroides_soja.json")
+  }
+  if(TIPO_PRODUCTO == "tofu") {
+    max_min <- fromJSON("max_min_tofu.json")
+    centroides <- fromJSON("centroides_tofu.json")
+  }
+  if(TIPO_PRODUCTO == "seitan") {
+    max_min <- fromJSON("max_min_seitan.json")
+    centroides <- fromJSON("centroides_seitan.json")
+  }
+  
+  nuevo_producto_normalizado <- data.frame(
+    ENERGY_100G =  normalize_column(DT_ENERGY_100G, max_min$ENERGY_100G$MAX, max_min$ENERGY_100G$MIN),
+    FAT_100G = normalize_column(DT_FAT_100G, max_min$FAT_100G$MAX, max_min$FAT_100G$MIN),
+    CARBOHYDRATES_100G = normalize_column(DT_CARBOHYDRATES_100G, max_min$CARBOHYDRATES_100G$MAX, max_min$CARBOHYDRATES_100G$MIN),
+    PROTEINS_100G = normalize_column(DT_PROTEINS_100G, max_min$PROTEINS_100G$MAX, max_min$PROTEINS_100G$MIN)
+  )
+  
+  # Asegurarse de que las columnas del nuevo producto y los centroides coinciden
+  if (!all(names(nuevo_producto_normalizado) %in% names(centroides))) {
+    stop("Las columnas del nuevo producto y los centroides deben coincidir.")
+  }
+  
+  # Calcula las distancias euclidianas entre el nuevo producto y cada centroide
+  distancias <- apply(centroides, 1, function(centroide) {
+    suma_cuadrados <- sum((nuevo_producto_normalizado - centroide)^2)
+    sqrt(suma_cuadrados)
+  })
+  
+  # Encuentra el índice del centroide más cercano
+  indice_centroide_cercano <- which.min(distancias)
+  
+  # Identificar el color y la descripción del cluster
+  color_descripcion <- descripciones_clusters[[TIPO_PRODUCTO]][indice_centroide_cercano]
+  
+  # Devolver el mensaje y las características del centroide
+  # FASF cambio al conformar la lista que se retorna
+  lista <- list(
+    as.character(names(color_descripcion)),
+    as.character(color_descripcion[as.character(names(color_descripcion))][1])
+  )
+  
+  # FASF se agrega el retorno de la lista de respuesta
+  return(lista)
+  
+}
+
+
+
